d53fc45f71a115f7f75e6bdad681958e8df42550
VIJAY GH <mrvijaygh@rediffmail.com>
2019-11-16 13:02:53 +0530

    Dev files

---
+++ reader.py
@@ -0,0 +1,13 @@
+#Author: Vijay GH, Created: 2019 NOV
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+try:
+        id, text = reader.read()
+        print(id)
+        print(text)
+finally:
+        GPIO.cleanup()
